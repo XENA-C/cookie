118a54464294d203e24a5387b59e0bf195bc08cc
XENA-C <123154792+XENA-C@users.noreply.github.com>
2023-12-30 17:27:13 +0900

    cookie Test

--- src/main/java/com/example/cookie/service/LoginService.java
+++ src/main/java/com/example/cookie/service/LoginService.java
@@ -21,19 +21,19 @@ public class LoginService {
             var id = loginRequest.getId();
             var pw = loginRequest.getPassword();
 
-            var optionalUser = userRepository.findByName(id);
-            //이름을 찾아서 로그인 id 전달 ???
+            var optionalUser = userRepository.findByName(id); //로그인 id
 
         if(optionalUser.isPresent()){ //사용자정보 찾으면 Cookie 저장
 
                 var userDto = optionalUser.get();
 
-                if (userDto.getPassword().equals(pw)){ //cookie에 해당 정보 저장
+                if (userDto.getPassword().equals(pw)){
+                    //cookie에 해당 정보 저장
                     var cookie = new Cookie("authorization-cookie", userDto.getId());
                     cookie.setDomain("localhost"); //특정 도메인에서만 쿠키 사용
                     cookie.setPath("/"); //특정 경로 설정
-//                    cookie.setHttpOnly(true); //JavaScript 에서 쿠키정보를 읽을 수 없도록 보안처리
-//                    cookie.setSecure(true); // Https 에서만 사용(Http X)
+//                  cookie.setHttpOnly(true); //JavaScript 에서 쿠키정보를 읽을 수 없도록 보안처리
+//                  cookie.setSecure(true); // Https 에서만 사용(Http X)
                     cookie.setMaxAge(-1); //사용기한 (-1: 세션 유지될 동안)
 
                     httpServletResponse.addCookie(cookie); //컨트롤러에서 넘어온 http리스폰스에 쿠키 추가
